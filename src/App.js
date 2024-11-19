bed3394d0a4ac64af91c6001e32a1d44c7af4531
CleProgrammer <clebsonmengao@hotmail.com>
2024-11-19 13:21:55 -0300

    bootstrap-project-finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import LogoIcon from './images/cb-icon.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark d-flex align-items-center justify-content-center" style={{width: "100vw", height: "100vh"}}>
+      <div className='w-50 h-75 d-flex align-items-center justify-content-center'>
+        <form className='w-50 h-50'>
+          <div className='w-100 d-flex align-items-center justify-content-center' style={{marginBottom:'35px'}}><img className='img-fluid' src={LogoIcon}/></div>
+          <h1 className='fs-6 text-light'>Login here</h1>
+          <div>
+            <input className='w-100' placeholder='Email'/>
+            <input className='w-100' placeholder='Senha'/>
+          </div>
+          <button type="button" className="btn btn-primary w-100" style={{marginTop: '10px'}} >Login</button>
+        </form>
+      </div>
     </div>
   );
 }
